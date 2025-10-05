d6fce2b4554d5704d0f7d58fda8ff2484681aa3b
Axel Vazquez <axelvazmar0@gmail.com>
2025-10-05 00:23:32 -0600

    Añadiendo el modelo Post

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,5 +1,46 @@
 from django.db import models
+from django.utils import timezone
+
+def blog_thumnail_directory(instance, filename):
+    """
+    Function to create a folder for each image of each
+    blog post (if the post has images/thumnails)
+    """
+    return "blog/{0}/{1}".format(instance.title, filename)
 
 # Create your models here.
 class Post:
-    pass
+
+    class PostObjects(models.Manager):
+        """
+        Class to get only the 'published' posts in the FE
+        """
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
+    status_options = (
+        ("draft", "Draft")
+        ("published", "Published")
+    )
+    
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=256)
+    content = models.TextField()
+    thumbnail = models.ImageField(upload_to=blog_thumnail_directory)
+
+    keywords = models.CharField(max_length=128)
+    slug = models.CharField(max_length=128)
+
+    created_at = models.DateField(default=timezone.now)
+    updated_at = models.DateField(auto_now=True)
+
+    status = models.CharField(max_length=10, choices=status_options,default='draft')
+
+    objects = models.Manager() # Default manager
+    post_objects = PostObjects() # Custom manager
+
+    class Meta:
+        ordering = ("-published")
+
+    def __str__(self):
+        return self.title
